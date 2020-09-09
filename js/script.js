26ebd44d18811ea2d36335c7bc26aa52beecc031
Alberto Zordan <zorberto@gmail.com>
2020-09-09 16:05:36 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Completare l’esercizio iniziato a lezione sulla todo-list.
+Utilizzare l’API di esempio http://157.230.17.132:3035/todos
+e fare le 4 operazioni Create, Read, Update e Delete. */
+
+$(document).ready(function () {
+
+});
